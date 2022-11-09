5e172b7947fcb3ca2084676dc4c6dc7eea9b9bd0
Synmad <lucasalumlucasalumlucasalum@gmail.com>
2022-11-09 18:07:47 -0300

    Proyecto importado

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float mouseSensitivity = 3f;
+    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float mass = 1f;
+    [SerializeField] Transform camaraTransform;
+
+    CharacterController characterController;
+    Vector3 velocity;
+    Vector2 look;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        UpdateLook();
+        UpdateMovement();
+        UpdateGravity();
+    }
+
+    void UpdateGravity()
+    {
+        var gravity = Physics.gravity * mass * Time.deltaTime;
+        velocity.y = characterController.isGrounded ? -1f : velocity.y + gravity.y;
+    }
+
+    void UpdateLook()
+    {
+        look.x += Input.GetAxis("Mouse X") * mouseSensitivity;
+        look.y += Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        // Prevents vertical camera movement from breaking the player's neck
+        look.y = Mathf.Clamp(look.y, -89f, 89f);
+
+        camaraTransform.localRotation = Quaternion.Euler(-look.y, 0, 0);
+        transform.localRotation = Quaternion.Euler(0, look.x, 0);
+    }
+
+    void UpdateMovement()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        var input = new Vector3();
+        input += transform.forward * y;
+        input += transform.right * x;
+        
+        // Prevents diagonal movement from being faster
+        input = Vector3.ClampMagnitude(input, 1f);
+
+        characterController.Move((input * movementSpeed + velocity) * Time.deltaTime);
+    }
+}
