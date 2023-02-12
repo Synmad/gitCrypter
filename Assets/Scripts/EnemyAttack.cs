77c36349166f481f1da81cbd36a257deaa7ccfef
Synmad <lucasalumlucasalumlucasalum@gmail.com>
2023-02-12 18:18:02 -0300

    v0.0.15b
    aaaaaaaaaaaa

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float range = 1f;
+    public Transform attackPoint;
+    public LayerMask playerLayer;
+
+    bool PlayerFound;
+
+    public float damage = 1f;
+
+    void Update()
+    {
+        PlayerFound = Physics.CheckSphere(attackPoint.position, range, playerLayer);
+
+        if (PlayerFound)
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+
+    }
+}
