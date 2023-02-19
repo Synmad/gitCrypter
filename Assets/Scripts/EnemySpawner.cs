aa12d7ad0bb1a444cb89a6d6a6053d52de27654f
Synmad <lucasalumlucasalumlucasalum@gmail.com>
2023-02-19 16:13:55 -0300

    v0.0.17
    (roto) Spawner enemigo.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemy;
+    [SerializeField] int xPosition;
+    [SerializeField] int zPosition;
+    [SerializeField] int enemyCount;
+
+
+    void Start()
+    {
+        StartCoroutine(enemySpawn());
+    }
+
+    IEnumerator enemySpawn()
+    {
+        while (enemyCount < 15)
+        {
+            xPosition = Random.Range(-17, 17);
+            zPosition = Random.Range(9, 19);
+            Instantiate(enemy, new Vector3(xPosition, 0, zPosition), Quaternion.identity);
+            yield return new WaitForSeconds(0.001f);
+            enemyCount += 1;
+        }
+    }
+        
+    }
