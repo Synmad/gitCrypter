1489ed27d23e73fe58afb3002adbd498380f50ac
Synmad <lucasalumlucasalumlucasalum@gmail.com>
2023-03-05 20:16:18 -0300

    v0.1.5a
    Sistema de spawn aleatorio reworkeado.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -14,6 +14,8 @@ public class EnemySpawner : MonoBehaviour
 
     public int enemyCount;
 
+    [SerializeField] Transform[] spawnPoints;
+
     private void Awake()
     {
         spawnTime = 10;
@@ -26,29 +28,33 @@ public class EnemySpawner : MonoBehaviour
 
     IEnumerator enemySpawn()
     {
-        while (enemyCount < 8)
+        
+
+        while (enemyCount < 10)
         {
-            xPosition = Random.Range(-17, 17);
-            zPosition = Random.Range(9, 19);
-            Instantiate(enemy, new Vector3(xPosition, 0, zPosition), Quaternion.identity);
+            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+            //xPosition = Random.Range(-17, 17);
+            //zPosition = Random.Range(9, 19);
+            //Instantiate(enemy, new Vector3(xPosition, 0, zPosition), Quaternion.identity);
+            Instantiate(enemy, spawnPoints[spawnPointIndex].position, Quaternion.identity);
             enemyCount += 1;
-            yield return new WaitForSeconds(spawnTime);
+            yield return new WaitForSeconds(1f);
         }
     }
 
     private void Update()
     {
-        if (spawnTime > middleSpawnTime)
-        {
-            spawnTime -= difficulty;
-            difficulty += 0.0001f * Time.deltaTime;
-        }
+        //if (spawnTime > middleSpawnTime)
+        //{
+        //    spawnTime -= difficulty;
+        //    difficulty += 0.00001f * Time.deltaTime;
+        //}
 
-        if (spawnTime < middleSpawnTime && spawnTime > minSpawnTime)
-        {
-            spawnTime -= difficulty;
-            difficulty += 0.000001f * Time.deltaTime;
-        }
-        
+        //if (spawnTime < middleSpawnTime && spawnTime > minSpawnTime)
+        //{
+        //    spawnTime -= difficulty;
+        //    difficulty += 0.000001f * Time.deltaTime;
+        //}
+
     }
 }
